b394e6a9807ce4a0f1fa6a7bd6c00c1323d4952b
yunimarketinghh-ship-it <yuni.marketing.hh@gmail.com>
2026-04-23 12:14:00 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://ffylxadhegvvwxrmyktt.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmeWx4YWRoZWdddnd4cm15a3R0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ0MDAwNTcsImV4cCI6MjAyOTk4MDA1N30.fake_key_for_demo'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmeWx4YWRoZWd2dnd4cm15a3R0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMzk3MDEsImV4cCI6MjA4ODgxNTcwMX0.VfhzFiX0acJ1NPK4eB-EbouKllguH188JK9pAGpWobs'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
